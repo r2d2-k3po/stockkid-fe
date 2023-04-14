d56585ae255992093dbc064f81195901309666e4
r2d2-k3po <r2d2factory3@gmail.com>
2023-04-14 12:50:05 +0900

    language provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,22 @@
-import React, {useState} from 'react';
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import SymbolTest from './tests/SymbolTest';
 import ReactDaisyuiTest from './tests/ReactDaisyuiTest';
 import LightDarkSwap from './components/LightDarkSwap';
 import LanguageSelect from './components/LanguageSelect';
-import {LanguageContext} from './contexts/LanguageContext';
+import {LanguageProvider} from './contexts/LanguageContext';
 import LanguageContextTest from './tests/LanguageContextTest';
 
 function App() {
-  const [language, setLanguage] = useState<string>(
-    localStorage.getItem('language') || 'ko'
-  );
-
   return (
-    <LanguageContext.Provider value={{language, setLanguage}}>
+    <LanguageProvider>
       <LanguageSelect />
       <LanguageContextTest />
       <LightDarkSwap />
       <SymbolTest />
       {/*<ReactDaisyuiTest />*/}
-    </LanguageContext.Provider>
+    </LanguageProvider>
   );
 }
 
--- src/contexts/LanguageContext.tsx
+++ src/contexts/LanguageContext.tsx
@@ -1,4 +1,4 @@
-import React, {createContext} from 'react';
+import React, {createContext, FC, useState} from 'react';
 
 export type LanguageContextType = {
   language: string;
@@ -6,3 +6,18 @@ export type LanguageContextType = {
 };
 
 export const LanguageContext = createContext<LanguageContextType | null>(null);
+
+type LanguageProviderProps = {
+  children: React.ReactNode;
+};
+export const LanguageProvider: FC<LanguageProviderProps> = ({children}) => {
+  const [language, setLanguage] = useState<string>(
+    localStorage.getItem('language') || 'ko'
+  );
+
+  return (
+    <LanguageContext.Provider value={{language, setLanguage}}>
+      {children}
+    </LanguageContext.Provider>
+  );
+};
